b12400d91149957db2733e9c352ab9c458d3c720
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-24 18:03:31 +0000

    Mise en place des différents state

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
-import Loader from "@/components/ui/loader";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [originalImageSrc, setOriginalImageSrc] = useState<string | null>(null);
+  const [compressedImageSrc, setCompressedImageSrc] = useState<string | null>(null);
+  const [originalSize, setOriginalSize] = useState<number>(0);
+  const [compressedSize, setCompressedSize] = useState<number | null>(null);
+  const [imageName, setImageName] = useState<string>("");
+  const [imageFormat, setImageFormat] = useState<string>("jpeg");
+  const [compressionType, setCompressionType] = useState<string>("lossy");
+
+  const [loading, setLoading] = useState<boolean>(false);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       BODY
