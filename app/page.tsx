5c89fa1d794ab091798f75b41d2a533730c98f75
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-24 18:07:38 +0000

    Mise en place du loader sur la page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [originalImageSrc, setOriginalImageSrc] = useState<string | null>(null);
-  const [compressedImageSrc, setCompressedImageSrc] = useState<string | null>(null);
+  const [compressedImageSrc, setCompressedImageSrc] = useState<string | null>(
+    null
+  );
   const [originalSize, setOriginalSize] = useState<number>(0);
   const [compressedSize, setCompressedSize] = useState<number | null>(null);
   const [imageName, setImageName] = useState<string>("");
@@ -12,6 +14,15 @@ export default function Home() {
   const [compressionType, setCompressionType] = useState<string>("lossy");
 
   const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (loading) {
+      const timer = setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [loading]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
