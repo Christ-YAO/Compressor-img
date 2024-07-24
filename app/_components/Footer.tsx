3e7f40a5f535ec880b0dda824ef26ec59a95392d
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-24 17:54:48 +0000

    correct some things

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-white flex items-center justify-center gap-2 flex-col w-full p-5 border-t border-gray-300">
+    <footer className="bg-white flex items-center justify-center gap-2 flex-col w-full p-5 border-t border-accent">
       <p className="text-sm text-center font-light">
         &copy;{" "}
         <Link
--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -4,15 +4,17 @@ import { Button } from "@/components/ui/button";
 
 export default function Nav() {
   return (
-    <nav className="max-w-[1000px] mx-auto border-b border-gray-300 h-[50px] flex items-center justify-between p-5">
-      <Link href={"/"}>
-        <Images color="red" />
-      </Link>
-      <Link target="_blank" href="https://github.com/nextui-org/nextui">
-        <Button variant="ghost">
-          <Github size={20}  />
-        </Button>
-      </Link>
-    </nav>
+    <header className="fixed z-20 top-0 w-full backdrop-blur-xl">
+      <nav className="max-w-[1000px] mx-auto border-b border-accent h-[50px] flex items-center justify-between p-5">
+        <Link href={"/"}>
+          <Images color="red" />
+        </Link>
+        <Link target="_blank" href="https://github.com/nextui-org/nextui">
+          <Button variant="ghost">
+            <Github size={20} />
+          </Button>
+        </Link>
+      </nav>
+    </header>
   );
 }
